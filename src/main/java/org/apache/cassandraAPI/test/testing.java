a0317386ca5241532c8e92fdf5c3527cf738c539
shashanks2008 <s.srivastava@student.tue.nl>
2017-03-17 13:04:18 +0100

    CassandraFlink API Modified the testing file, now arguments can be passed in the command line while running Jar, a script is added to do experiments with different combinations of rows and columns.

--- src/main/java/org/apache/cassandraAPI/test/testing.java
+++ src/main/java/org/apache/cassandraAPI/test/testing.java
@@ -1,13 +1,13 @@
 package org.apache.cassandraAPI.test;
 
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.WriteFormatAsCsv;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Vector;
 
@@ -25,12 +25,14 @@ public class testing {
         StreamExecutionEnvironment streamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
         /*To read the file from stream Buffer reader is used*/
-        BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Big Data\\Data\\test.csv")));
+       // BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Big Data\\Data\\test.csv")));
         String line;
         /* Creating ArrayList of Vector, which will store all the elements from the file*/
         ArrayList<Vector<Double>> states5 = new ArrayList<Vector<Double>>();
-        int numcol = 10;
-        for(int k = 1; k < 10000; k++){
+        Integer numrow = Integer.parseInt(args[0]);
+        Integer numcol = Integer.parseInt(args[1]);
+
+        for(int k = 1; k <= numrow; k++){
             Vector<Double> vector = new Vector<Double>(numcol);
             for (int v =0 ; v<=numcol-1; v++)
             {
@@ -38,17 +40,68 @@ public class testing {
             }
             states5.add(vector);
         }
-        int sizeoffile = states5.size();
+        Integer sizeoffile = states5.size();
+        //DataSet<Tuple4<Integer, Integer, Double, Double>> elements = null;
+
+        Double time = 0.0;
+        Double[] duration = new Double[10];
+        Double[] std = new Double[10];
+        Integer experiment =Integer.parseInt(args[2]);
+        Double st_dev = 0.0;
         ArrayList<Tuple4<Integer, Integer,Double,Double>> collection1 = addFunStr(states5);
         /*Calling the function SetQuery, which accept ArrayList and executionEnvironment*/
-        long startTime = System.currentTimeMillis();
+        for(int exp =0 ; exp <experiment ;exp++) {
+            long startTime = System.currentTimeMillis();
 
-      setQuery(collection1,streamExecutionEnvironment);
-       streamExecutionEnvironment.execute("Write");
+ /*Save into Cassandra using DataSet*/
+//        setQueryDataSet(collection1,env);
+//           env.execute("Write");
+
+            /*Save into Cassandra using Cassandra Sink Streaming*/
+        setQuery(collection1,streamExecutionEnvironment);
+           streamExecutionEnvironment.execute("Write");
         /*Uncomment for retrieving from the table*/
-        //getQuery3(env).print();
-        long endTime = System.currentTimeMillis();
-        long duration = (endTime - startTime);  //divide by 1000000 to get milliseconds.
-        System.out.println("Execution time for the Set method is: " + duration +" milliseconds and size of file is " +sizeoffile*numcol*2 + " Byte" );
+//            getQuery3(env, args[0]).print();
+
+            long endTime = System.currentTimeMillis();
+            duration[exp] = Double.valueOf((endTime - startTime));  //divide by 1000000 to get milliseconds.
+            time = time+duration[exp];
+            //System.out.println("Execution time for the Set method is: " + duration + " milliseconds and size of file is " + sizeoffile * numcol * 2 + " Byte");
+        }
+        Double meantime = time/experiment;
+        for (int ex=0; ex<experiment;ex++){
+            std[ex] = Math.pow((duration[ex] -  (meantime)),2);
+            st_dev = st_dev + std[ex];
+        }
+        Double standardDev = Math.sqrt(st_dev)/experiment;
+        System.out.println("Average execution time for the method is: " + meantime +" milliseconds and Standard Deviation is " + standardDev +"  " + sizeoffile * numcol * 2 + " Byte");
+        Integer memorySize = sizeoffile * numcol * 2;
+//        ArrayList<Tuple4<Double, Double,Integer, Integer>> datatoCSV= new ArrayList<Tuple4<Double, Double,Integer, Integer>>();
+//        datatoCSV.add(new Tuple4<Double, Double,Integer, Integer> (meantime, standardDev,memorySize ,experiment));
+
+        //Open File
+        BufferedWriter writer = null;
+        try {
+            //create a temporary file
+//            String timeLog = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+            File logFile = new File("MyFile");
+
+            // This will output the full path where the file will be written to...
+//            System.out.println(logFile.getCanonicalPath());
+
+            writer = new BufferedWriter(new FileWriter(logFile));
+            writer.write(Integer.toString(memorySize)+","+Integer.toString(experiment)+","+Integer.toString(numrow)+","+Integer.toString(numcol)+","+Double.toString(meantime)+","+Double.toString(standardDev)+"\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // Close the writer regardless of what happens...
+                writer.close();
+            } catch (Exception e) {
+            }
+        }
+
+
+
     }
 }
